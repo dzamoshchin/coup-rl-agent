41cd3f43e26f8f1a0b301806f4fdf07e117715b9
dzamoshchin <danzamoshchin@gmail.com>
2023-03-01 20:32:36 -0800

    initial commit

---
+++ player.py
@@ -0,0 +1,13 @@
+class Player:
+
+    def __init__(self):
+        pass
+
+    def move(self) -> str:
+        pass
+
+    def respond(self) -> str:
+        pass
+
+    def get_observation(self):
+        pass
