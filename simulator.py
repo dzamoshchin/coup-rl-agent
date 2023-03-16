70f5f0fecbe6f42d25f1854ff3db33c59fa5e41c
dzamoshchin <danzamoshchin@gmail.com>
2023-03-16 01:54:12 -0700

    minor

--- simulator.py
+++ simulator.py
@@ -299,28 +299,7 @@ if __name__ == '__main__':
     rates = []
     for i in range(50000):
         # sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, HeuristicPlayer], params=[{}, {}, {}, {}], verbosity=0)
-        # sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, MCTSPlayer], params=[{}, {}, {}, {'Q':Q, 'N':N, 'c':.01, 'depth':100, 'num_simulations':50, 'alpha':0.1}], verbosity=0)
-        sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, QPlayer], params=[{}, {}, {}, {'Q':Q, 'N':N, 'c':.01, 'alpha':0.1}], verbosity=0)
-        winner = sim.run_game()
-        winners[winner] += 1
-        if i % 1000 == 0:
-            print(i)
-            print(winners)
-            print(winners - last)
-            print((winners[3] / np.sum(winners)) * 100)
-            print(((winners-last)[3] / np.sum(winners-last)) * 100)
-            rates.append(((winners-last)[3] / np.sum(winners-last)) * 100)
-            last = np.copy(winners)
-    # plt.plot(rates[1:])
-    # plt.show()
-    # print()
-
-    winners = np.array([0, 0, 0, 0])
-    last = np.copy(winners)
-    rates = []
-    for i in range(50000):
-        # sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, HeuristicPlayer], params=[{}, {}, {}, {}], verbosity=0)
-        sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, MCTSPlayer], params=[{}, {}, {}, {'Q':Q, 'N':N, 'c':.01, 'depth':10, 'num_simulations':20, 'alpha':0.1}], verbosity=0)
+        sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, MCTSPlayer], params=[{}, {}, {}, {'Q':Q, 'N':N, 'c':.01, 'depth':100, 'num_simulations':50, 'alpha':0.1}], verbosity=0)
         # sim = Simulator.from_start([RandomPlayer, RandomPlayer, RandomPlayer, QPlayer], params=[{}, {}, {}, {'Q':Q, 'N':N, 'c':.01, 'alpha':0.1}], verbosity=0)
         winner = sim.run_game()
         winners[winner] += 1
